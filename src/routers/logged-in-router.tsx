d0c446d86b5514ab326c0163ad288f64c9fb80da
aureuneun <aureuneun@gmail.com>
2020-12-15 10:10:35 +0900

    feat: Local-only fields

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { isLoggedInVar } from '../apollo';
 
 export const LoggedInRouter = () => {
-  return <h1>Logged In</h1>;
+  return (
+    <>
+      <h1>Logged In</h1>
+      <button onClick={() => isLoggedInVar(false)}>Click to logout</button>
+    </>
+  );
 };
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { isLoggedInVar } from '../apollo';
 
 export const LoggedOutRouter = () => {
-  return <h1>Logged out</h1>;
+  return (
+    <>
+      <h1>Logged out</h1>
+      <button onClick={() => isLoggedInVar(true)}>Click to login</button>
+    </>
+  );
 };
