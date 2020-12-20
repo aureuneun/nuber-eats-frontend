3a43d49efcb9e72a9eb5b2248a10f931055d466d
aureuneun <aureuneun@gmail.com>
2020-12-20 23:16:13 +0900

    fix: restaurant pages

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -40,24 +40,24 @@ export const Restaurant = () => {
         <title>Restaurant</title>
       </Helmet>
       <div
-        className="bg-red-200 py-36 bg-cover bg-center opacity-80"
+        className="bg-red-200 py-32 bg-cover bg-center"
         style={{
           backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
         }}
       >
-        <div className="bg-white w-52 py-6 pl-16">
+        <div className="bg-white w-80 py-8 pl-16 ml-2">
           <h3 className="text-3xl">{data?.restaurant.restaurant?.name}</h3>
-          <h5 className="text-sm font-light my-2">
+          <h5 className="text-xs font-light mt-2 mb-1">
             <Link
               to={`/category/${data?.restaurant.restaurant?.category?.name
                 .trim()
                 .toLowerCase()
                 .replace(/ /g, '-')}`}
             >
-              {data?.restaurant.restaurant?.category?.name}
+              {`• ${data?.restaurant.restaurant?.category?.name}`}
             </Link>
           </h5>
-          <h5 className="text-sm font-light">
+          <h5 className="text-base font-light">
             {data?.restaurant.restaurant?.address}
           </h5>
         </div>
